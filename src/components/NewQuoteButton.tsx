20aee3ffbc803dc9336d8fa0c496a4451f6dbf93
Yamasaki Junpei <opss0622@gmail.com>
2023-04-23 15:20:47 +0000

    fix: 不要な記述を削除

--- src/components/NewQuoteButton.tsx
+++ src/components/NewQuoteButton.tsx
@@ -1,15 +1,7 @@
-import { useGetAnimeQuotesQuery } from "../redux/animeQuotesApi";
-
 export function NewQuoteButton() {
-  const { data, error, isFetching } = useGetAnimeQuotesQuery("one piece");
-	
-	return (
-		<button
-			type="button"
-			className="btn primary"
-			// onClick={dispatch()}
-		>
-			New Quote
-		</button>
-	);
+  return (
+    <button type="button" className="btn primary">
+      New Quote
+    </button>
+  );
 }
