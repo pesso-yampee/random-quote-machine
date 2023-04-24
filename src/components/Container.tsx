77afff980d02f19e6db3ccd9bee147c87b6be066
yamasaki junpei <opss0622@gmail.com>
2023-04-24 14:56:12 +0900

    feat: ローディングアイコンの追加

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,6 @@
 import type { RootState } from "../redux/store";
 import { useGetAnimeQuotesQuery } from "../redux/animeQuotesApi";
+import LoadingIcons from "react-loading-icons";
 export function Container() {
   const { data, error, isLoading, isFetching, refetch } =
     useGetAnimeQuotesQuery("one piece");
@@ -7,10 +8,16 @@ export function Container() {
   return (
     <div className="container">
       <div id="quote-box" className="quoteBox">
-        <p className="text">{data?.quote}</p>
-        <div id="author" className="author">
-          <span>{data?.character}</span>
-        </div>
+        {isLoading || isFetching ? (
+          <LoadingIcons.ThreeDots fill="#228b22" speed={0.75} />
+        ) : (
+          <>
+            <p className="text">{data?.quote}</p>
+            <div id="author" className="author">
+              <span>{data?.character}</span>
+            </div>
+          </>
+        )}
         <div className="flexContainer">
           <button type="button" className="btn primary" onClick={refetch}>
             New Quote
